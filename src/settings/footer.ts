e8c0bf481abe95f5b8207d4f6bf812c00270afa9
0nikod <dniko0@outlook.com>
2026-02-23 13:34:53 +0800

    chore: 修正 footer.ts

---
+++ src/settings/footer.ts
@@ -0,0 +1,21 @@
+export interface SocialLink {
+  ariaLabel: string;
+  icon: string;
+  url: string;
+}
+
+export interface FooterConfig {
+  author: string;
+  socialLinks: SocialLink[];
+}
+
+export const footerConfig: FooterConfig = {
+  author: "PaperAir",
+  socialLinks: [
+    {
+      ariaLabel: "Github Profile",
+      icon: "ri:github-fill",
+      url: "https://github.com",
+    },
+  ],
+};
